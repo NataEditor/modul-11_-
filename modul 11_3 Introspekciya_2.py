c5c102754f2e1714434351701626a20461faa90d
NataEditor <nako_al@mail.ru>
2024-11-12 16:31:31 +0300

    доработка кода
    отсеивание атрибутов

---
+++ modul 11_3 Introspekciya_2.py
@@ -0,0 +1,19 @@
+import inspect
+
+
+
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
+    module = obj.__class__.__module__
+    dop=inspect.ismodule(obj) 
+    dop2=inspect.isclass(obj)
+
+    info = {'Тип объекта': obj_type, 'Атрибуты объекта': attributes, 'Методы объекта': methods, 'Модуль, к которому объект принадлежит': module, 'Является ли объект модулем': dop, 'Является ли объект классом': dop2}
+    
+    return info
+
+
+number_info = introspection_info(42)
+print(number_info)
